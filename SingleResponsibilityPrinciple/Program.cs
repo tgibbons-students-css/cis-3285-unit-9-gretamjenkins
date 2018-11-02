08a210770f4f983e12975168e61079d138cc3101
Greta Jenkins <gjenkins@css.edu>
2018-11-02 11:17:20 -0500

    Request 407 - created a new data provider that uses URLs to get data instead of having to use local files and an IO stream

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -9,11 +9,12 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            // URL source for the trade data to be provided
+            string URL = "http://faculty.css.edu/tgibbons/trades4.txt";
 
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var tradeDataProvider = new URLTradeDataProvider(URL);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SingleResponsibilityPrinciple.Contracts;
+
+namespace SingleResponsibilityPrinciple
+{
+    class URLTradeDataProvider : ITradeDataProvider
+    {
+        public URLTradeDataProvider(string URL)
+        {
+            this.URL = URL;
+        }
+
+        // Gets the trade data from an online file using a string URL
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            // create new web client and use it to read the file stored at the given URL
+            var client = new WebClient();
+            using (var stream = client.OpenRead(URL))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
+        // URL to be used for data provision
+        private readonly string URL;
+    }
+
+}
