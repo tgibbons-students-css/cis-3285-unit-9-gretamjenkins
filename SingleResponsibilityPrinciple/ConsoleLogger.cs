4a5c73a24e50f1086ae2a41fd476e32a34d0d37f
Greta Jenkins <gjenkins@css.edu>
2018-11-02 11:06:54 -0500

    Request 405 - modified the ConsoleLogger class to log all messages to an xml file in the debug folder

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
@@ -8,12 +8,22 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
+            LogToFile("WARN", message);
             Console.WriteLine(string.Concat("WARN: ", message), args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
+            LogToFile("INFO", message);
             Console.WriteLine(string.Concat("INFO: ", message), args);
+        }
+
+        public void LogToFile(string message, string messageType)
+        {
+            using (StreamWriter logger = File.AppendText("log.xml"))
+            {
+                logger.WriteLine("<log><type>" + messageType + "</type><message>" + message + "</message></log>");
+            }
         }
     }
 }
